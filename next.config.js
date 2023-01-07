77c78e04dd7d496d18a2314e57229dd60df18e6a
katy.garner <katy.garner@geekgirlsolutions.com>
2023-01-07 00:42:16 +0000

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+module.exports = { reactStrictMode: true };
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+module.exports = withPWA({});
